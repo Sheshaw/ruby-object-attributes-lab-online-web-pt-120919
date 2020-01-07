7763a83f68772bb1d5a742d50137cd7f8b8616f6
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-07 23:48:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,11 +2,18 @@ class Dog
   
   
   def name=(the_dog_name)
-     @dog_name = dog_name
+     @dog_name = "Joey"
   end
   
-  def name(x)
+  def name
     @dog_name
   end
-
+  
+  def breed=(command_for_the_dog_to_breed)
+    @breed_command = "Breed"
+  end
+  
+  def name
+    @breed_command
+  end
 end
